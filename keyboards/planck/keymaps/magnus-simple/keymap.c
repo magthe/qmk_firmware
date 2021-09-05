050eb88e8425601b36dcc68b645153d1acbd4f7a
Magnus Therning <magnus@therning.org>
2021-09-05 17:19:06 +0200

    Mirror the number layer
    Let's see if it's easier to do numbers using the left hand.

--- keyboards/planck/keymaps/magnus-simple/keymap.c
+++ keyboards/planck/keymaps/magnus-simple/keymap.c
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
       LT(NUM,KC_CAPS), KC_LCTL, KC_LGUI, KC_LALT, LT(NAV,KC_ESC),
       LT(SYM,KC_SPC),
-      KC_ENT, KC_RALT, KC_RGUI, KC_RCTL, LT(FUN,KC_APP)),
+      LT(NUM,KC_ENT), KC_RALT, KC_RGUI, KC_RCTL, LT(FUN,KC_APP)),
 
     /* Navigation
      * ,-----------------------------------------------------------------------------------.
@@ -109,20 +109,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* Numbers
      * ,-----------------------------------------------------------------------------------.
-     * |      |      |      |      |      |      |      |  1   |  2   |  3   |  4   |      |
+     * |      |  1   |  2   |  3   |  4   |      |      |  1   |  2   |  3   |  4   |      |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * |      |      |      |      |      |      |      |  5   |  6   |  7   |  8   |      |
+     * |      |  5   |  6   |  7   |  8   |      |      |  5   |  6   |  7   |  8   |      |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * |      |      |      |      |      |      |      |  9   |  0   |      |      |      |
+     * |      |  9   |  0   |      |      |      |      |  9   |  0   |      |      |      |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * |  ▽  | Ctrl |  GUI | Alt  |      |             |      |      |      |      |      |
+     * |  ▽  | Ctrl |  Gui | Alt  |      |             |  ▽  | Alt  | Gui  | Ctrl |      |
      * `-----------------------------------------------------------------------------------'
      */
 	[NUM] = LAYOUT_planck_1x2uC(
-      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_1, KC_2, KC_3,  KC_4,  KC_NO,
-      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_5, KC_6, KC_7,  KC_8,  KC_NO,
-      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_9, KC_0, KC_NO, KC_NO, KC_NO,
+      KC_NO, KC_1, KC_2, KC_3,  KC_4,  KC_NO, KC_NO, KC_1, KC_2, KC_3,  KC_4,  KC_NO,
+      KC_NO, KC_5, KC_6, KC_7,  KC_8,  KC_NO, KC_NO, KC_5, KC_6, KC_7,  KC_8,  KC_NO,
+      KC_NO, KC_9, KC_0, KC_NO, KC_NO, KC_NO, KC_NO, KC_9, KC_0, KC_NO, KC_NO, KC_NO,
       KC_TRNS, KC_LCTL, KC_LGUI, KC_LALT, KC_NO,
       KC_NO,
-      KC_0, KC_NO, KC_NO, KC_NO, KC_NO),
+      KC_TRNS, KC_RALT, KC_RGUI, KC_RCTL, KC_NO),
 };
