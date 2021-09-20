38323b5b439a39db002d8606201f487d35b02b99
Magnus Therning <magnus@therning.org>
2021-09-20 18:27:38 +0200

    Remove the layer-switching on CapsLock, I don't use it
    It wasn't really usable with the digits on the left side. Also, the diagrams
    were corrected.

--- keyboards/planck/keymaps/magnus-simple/keymap.c
+++ keyboards/planck/keymaps/magnus-simple/keymap.c
@@ -38,15 +38,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------+------+------+------+------+------+------|
      * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * |      |      |      |      |  ESC |    Space    |      |      |      |      | Menu |
-     * | CapsL| Ctrl | GUI  | Alt  |(_NAV)|    (_SYM)   |  ENT | Alt  | GUI  | Ctrl |(_FUN)|
+     * |      |      |      |      |  ESC |    Space    |  ENT |      |      |      | Menu |
+     * | CapsL| Ctrl | GUI  | Alt  |(_NAV)|    (_SYM)   | _NUM | Alt  | GUI  | Ctrl |(_FUN)|
      * `-----------------------------------------------------------------------------------'
      */
 	[QWE] = LAYOUT_planck_1x2uC(
       KC_DEL,  KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I,    KC_O,   KC_P,    KC_BSPC,
       KC_TAB,  KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT,
       KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-      LT(NUM,KC_CAPS), KC_LCTL, KC_LGUI, KC_LALT, LT(NAV,KC_ESC),
+      KC_CAPS, KC_LCTL, KC_LGUI, KC_LALT, LT(NAV,KC_ESC),
       LT(SYM,KC_SPC),
       LT(NUM,KC_ENT), KC_RALT, KC_RGUI, KC_RCTL, LT(FUN,KC_APP)),
 
@@ -115,14 +115,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------+------+------+------+------+------+------|
      * |      |      |      |      |      |      |      |      |      |      |      |      |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * |  ▽  | Ctrl |  Gui | Alt  |      |             |  ▽  | Alt  | Gui  | Ctrl |      |
+     * |      | Ctrl |  Gui | Alt  |      |             |  ▽  | Alt  | Gui  | Ctrl |      |
      * `-----------------------------------------------------------------------------------'
      */
 	[NUM] = LAYOUT_planck_1x2uC(
       KC_NO, KC_1,  KC_2,  KC_3,  KC_4,  KC_5,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
       KC_NO, KC_6,  KC_7,  KC_8,  KC_9,  KC_0,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_9,  KC_0,  KC_NO, KC_NO, KC_NO,
-      KC_TRNS, KC_LCTL, KC_LGUI, KC_LALT, KC_NO,
+      KC_NO, KC_LCTL, KC_LGUI, KC_LALT, KC_NO,
       KC_NO,
       KC_TRNS, KC_RALT, KC_RGUI, KC_RCTL, KC_NO),
 };
