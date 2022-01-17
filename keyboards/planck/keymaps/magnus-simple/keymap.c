205300947c701f3168e583a6ab3639b233f0a048
Magnus Therning <magnus@therning.org>
2022-01-17 11:32:46 +0100

    Put LALT and RALT on the same key, RALT is compose

--- keyboards/planck/keymaps/magnus-simple/keymap.c
+++ keyboards/planck/keymaps/magnus-simple/keymap.c
@@ -1,3 +1,4 @@
+/* Build: qmk flash -kb planck/rev6 -km magnus-simple */
 #include QMK_KEYBOARD_H
 
 enum unicode_name {
@@ -38,17 +39,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------+------+------+------+------+------+------|
      * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * |      |      |      |      |  ESC |    Space    |  ENT |      |      |      | Menu |
-     * | CapsL| GUI  | Ctrl | Alt  |(_NAV)|    (_SYM)   | _NUM | Alt  | Ctrl | GUI  |(_FUN)|
+     * |      |      |      | RAlt |  ESC |    Space    |  ENT | RAlt |      |      | Menu |
+     * | CapsL| GUI  | Ctrl |(Alt) |(_NAV)|    (_SYM)   | _NUM |(Alt) | Ctrl | GUI  |(_FUN)|
      * `-----------------------------------------------------------------------------------'
      */
 	[QWE] = LAYOUT_planck_1x2uC(
       KC_DEL,  KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I,    KC_O,   KC_P,    KC_BSPC,
       KC_TAB,  KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT,
       KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-      KC_CAPS, KC_LGUI, KC_LCTL, KC_LALT, LT(NAV,KC_ESC),
+      KC_CAPS, KC_LGUI, KC_LCTL, LALT_T(KC_RALT), LT(NAV,KC_ESC),
       LT(SYM,KC_SPC),
-      LT(NUM,KC_ENT), KC_LALT, KC_RCTL, KC_LGUI, LT(FUN,KC_APP)),
+      LT(NUM,KC_ENT), LALT_T(KC_RALT), KC_RCTL, KC_LGUI, LT(FUN,KC_APP)),
 
     /* Navigation
      * ,-----------------------------------------------------------------------------------.
@@ -73,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ,-----------------------------------------------------------------------------------.
      * |      |  F1  |  F2  |  F3  |  F4  |  F5  |      |      |      |      |      |      |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * | RAlt |  F6  |  F7  |  F8  |  F9  |  F10 |      |      |      |      |      |      |
+     * |      |  F6  |  F7  |  F8  |  F9  |  F10 |      |      |      |      |      |      |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
      * |      |  F11 |  F12 |      |      |      |      |      |      |      |      |      |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -81,9 +82,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------------------------------'
      */
 	[FUN] = LAYOUT_planck_1x2uC(
-      KC_NO,   KC_F1,  KC_F2,  KC_F3, KC_F4, KC_F5, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-      KC_RALT, KC_F6,  KC_F7,  KC_F8, KC_F9, KC_F10, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-      KC_NO,   KC_F11, KC_F12, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_RSFT,
+      KC_NO, KC_F1,  KC_F2,  KC_F3, KC_F4, KC_F5,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+      KC_NO, KC_F6,  KC_F7,  KC_F8, KC_F9, KC_F10, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+      KC_NO, KC_F11, KC_F12, KC_NO, KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_RSFT,
       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
       KC_NO,
       KC_NO, KC_LALT, KC_RCTL, KC_LGUI, KC_TRNS),
