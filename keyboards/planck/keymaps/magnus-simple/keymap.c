7ff16732c9ddaa5e201eb69131df8f390b3affb0
Magnus Therning <magnus@therning.org>
2023-08-07 11:24:20 +0200

    Add SPC and ENT in a few places
    Turns out I'd missed the need for using them with modifiers.

--- keyboards/planck/keymaps/magnus-simple/keymap.c
+++ keyboards/planck/keymaps/magnus-simple/keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  |      |      | Alt  | Ctrl | GUI  |Shift |
    * |------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      |      |      |      |      |             |  ▽  |      |      |      |      |
+   * |      |      |      |      |      |    Space    |  ▽  |      |      |      |      |
    * `-----------------------------------------------------------------------------------'
    */
   [QWL] = LAYOUT_planck_1x2uC(
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,          KC_A, KC_S, KC_D, KC_F, KC_G, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
       KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_NO, KC_NO, KC_LALT, KC_RCTL, KC_LGUI, KC_RSFT,
       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-      KC_NO,
+      KC_SPC,
       KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO),
 
   /* Qwerty Right side
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+------+------+------+------+------|
    * | Shift| GUI  | Ctrl | Alt  |      |      |   N  |   M  |   ,  |   .  |   /  |Shift |
    * |------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      |      |      |      |  ▽  |             |      |      |      |      |      |
+   * |      |      |      |      |  ▽  |    Space    | ENT  |      |      |      |      |
    * `-----------------------------------------------------------------------------------'
    */
   [QWR] = LAYOUT_planck_1x2uC(
@@ -67,8 +67,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT,
       KC_LSFT, KC_LGUI, KC_LCTL, KC_LALT, KC_NO, KC_NO, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
       KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,
-      KC_NO,
-      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
+      KC_SPC,
+      KC_ENT, KC_NO, KC_NO, KC_NO, KC_NO),
 
   /* Navigation
    * ,-----------------------------------------------------------------------------------.
