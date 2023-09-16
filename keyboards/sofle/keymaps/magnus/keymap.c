de53f2f3ef7c219fe6b2d6cc78d64c7747e85887
Magnus Therning <magnus@therning.org>
2023-09-16 21:35:17 +0200

    Swap all right-alt for left-alt since I'm using it for compose

--- keyboards/sofle/keymaps/magnus/keymap.c
+++ keyboards/sofle/keymaps/magnus/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|-------.    ,-------|------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |       |    |       |   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------+-------|    |-------+-----------------------------------------'
- *               | LGUI | LAlt | LCTR |      |  SYM  |    |  SYM  |      | RCTR | RAlt | RGUI |
+ *               | LGUI | LAlt | LCTR |      |  SYM  |    |  SYM  |      | RCTR | LAlt | RGUI |
  *               |      |      |      |      | Space |    | Enter |      |      |      |      |
  *               `-----------------------------------'    `-----------------------------------'
  */
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                                             KC_Y,     KC_U,    KC_I,    KC_O,   KC_P,     KC_BSPC,
 		   KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                                             KC_H,     KC_J,    KC_K,    KC_L,   KC_SCLN,  KC_QUOT,
 		   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_NO,                   KC_NO,            KC_N,     KC_M,    KC_COMM, KC_DOT, KC_SLSH,  KC_RSFT,
-		                     KC_LGUI, KC_LALT, KC_LCTL, KC_NO, LT(_SYM, KC_SPC),        LT(_SYM, KC_ENT), MO(_NAV), KC_RCTL, KC_RALT, KC_RGUI
+		                     KC_LGUI, KC_LALT, KC_LCTL, KC_NO, LT(_SYM, KC_SPC),        LT(_SYM, KC_ENT), MO(_NAV), KC_RCTL, KC_LALT, KC_RGUI
 ),
 /* SYMBOLS
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|-------.    ,-------|------+------+------+------+------+------|
  * |      |      |      |   (  |   [  |   {  |       |    |       |   }  |   ]  |   )  |   =  |   \  |      |
  * `-----------------------------------------+-------|    |-------+-----------------------------------------'
- *               | LGUI | LAlt | LCTR |      |   ▽  |    |   ▽  |      | RCTR | RAlt | RGUI |
+ *               | LGUI | LAlt | LCTR |      |   ▽  |    |   ▽  |      | RCTR | LAlt | RGUI |
  *               `-----------------------------------'    `-----------------------------------'
  */
 [_SYM] = LAYOUT(
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TILD,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                         KC_CIRC, KC_AMPR, KC_ASTR, KC_PLUS, KC_PIPE, KC_UNDS,
   KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
   KC_NO,    KC_NO,   KC_NO,   KC_LPRN, KC_LBRC, KC_LCBR, KC_NO,         KC_NO,   KC_RCBR, KC_RBRC, KC_RPRN, KC_EQL,  KC_BSLS, KC_NO,
-                     KC_LGUI, KC_LALT, KC_LCTL, KC_NO,   KC_TRNS,       KC_TRNS, KC_NO,   KC_RCTL, KC_RALT, KC_RGUI
+                     KC_LGUI, KC_LALT, KC_LCTL, KC_NO,   KC_TRNS,       KC_TRNS, KC_NO,   KC_RCTL, KC_LALT, KC_RGUI
 ),
 /* NAVIGATION
  * ,----------------------------------------.                     ,-----------------------------------------.
