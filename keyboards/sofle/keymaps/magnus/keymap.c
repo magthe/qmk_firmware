bb2f5d06ef0141f693caf9f46cc1c0ffeade2b96
Magnus Therning <magnus@therning.org>
2023-09-16 21:08:10 +0200

    Turn the 'lower' layer into a layer with symbols ('sym')

--- keyboards/sofle/keymaps/magnus/keymap.c
+++ keyboards/sofle/keymaps/magnus/keymap.c
@@ -2,7 +2,7 @@
 
 enum sofle_layers {
     _QWERTY,
-    _LOWER,
+    _SYM,
     _RAISE,
 };
 
@@ -28,36 +28,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|-------.    ,-------|------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |       |    |       |   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------+-------|    |-------+-----------------------------------------'
- *               | LGUI | LAlt | LCTR |LOWER | Space |    | Enter |RAISE | RCTR | RAlt | RGUI |
+ *               | LGUI | LAlt | LCTR |      | SYM   |    | Enter |RAISE | RCTR | RAlt | RGUI |
+ *               |      |      |      |      | Space |    | Space |      |      |      |      |
  *               `-----------------------------------'    `-----------------------------------'
  */
 
 [_QWERTY] = LAYOUT(
- KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                              KC_6,       KC_7,    KC_8,    KC_9,   KC_0,     KC_BSLS,
- KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                              KC_Y,       KC_U,    KC_I,    KC_O,   KC_P,     KC_BSPC,
- KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                              KC_H,       KC_J,    KC_K,    KC_L,   KC_SCLN,  KC_QUOT,
- KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,       KC_NO,         KC_NO,  KC_N,       KC_M,    KC_COMM, KC_DOT, KC_SLSH,  KC_RSFT,
-                   KC_LGUI, KC_LALT, KC_LCTL, MO(_LOWER), KC_SPC,        KC_ENT, MO(_RAISE), KC_RCTL, KC_RALT, KC_RGUI
+		   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                                             KC_6,       KC_7,    KC_8,    KC_9,   KC_0,     KC_BSLS,
+		   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                                             KC_Y,       KC_U,    KC_I,    KC_O,   KC_P,     KC_BSPC,
+		   KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                                             KC_H,       KC_J,    KC_K,    KC_L,   KC_SCLN,  KC_QUOT,
+		   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_NO,                   KC_NO,            KC_N,       KC_M,    KC_COMM, KC_DOT, KC_SLSH,  KC_RSFT,
+		                     KC_LGUI, KC_LALT, KC_LCTL, KC_NO, LT(_SYM, KC_SPC),        LT(_SYM, KC_ENT), MO(_RAISE), KC_RCTL, KC_RALT, KC_RGUI
 ),
-/* LOWER
+/* SYMBOLS
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  | F11  |
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | F12  |
+ * |  ~   |   !  |   @  |   #  |   $  |   %  |                    |   ^  |   &  |   *  |   +  |   |  |  _   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |  ~   |   !  |   @  |   #  |   $  |   %  |                    |   ^  |   &  |   *  |   (  |   )  |   |  |
+ * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  -   |
  * |------+------+------+------+------+------|-------.    ,-------|------+------+------+------+------+------|
- * | Shift|  =   |  -   |  +   |   {  |   }  |       |    |       |   [  |   ]  |   ;  |   :  |   \  | Shift|
+ * |      |      |      |   (  |   [  |   {  |       |    |       |   }  |   ]  |   )  |   =  |   \  |      |
  * `-----------------------------------------+-------|    |-------+-----------------------------------------'
- *               | LGUI | LAlt | LCTR |LOWER | Space |    | Enter |RAISE | RCTR | RAlt | RGUI |
+ *               | LGUI | LAlt | LCTR |      |   ▽  |    |   ▽  |RAISE | RCTR | RAlt | RGUI |
  *               `-----------------------------------'    `-----------------------------------'
  */
-[_LOWER] = LAYOUT(
-  KC_NO,    KC_F1,   KC_F2, KC_F3,     KC_F4,   KC_F5,                           KC_F6,      KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-  KC_GRV,   KC_1,    KC_2,  KC_3,      KC_4,    KC_5,                            KC_6,       KC_7,    KC_8,    KC_9,    KC_0,    KC_F12,
-  KC_TILD,  KC_EXLM, KC_AT, KC_HASH,   KC_DLR,  KC_PERC,                         KC_CIRC,    KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
-  KC_LSFT,  KC_EQL,  KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR,    KC_NO,       KC_NO,  KC_LBRC,    KC_RBRC, KC_SCLN, KC_COLN, KC_BSLS, KC_RSFT,
-                     KC_LGUI, KC_LALT, KC_LCTL, MO(_LOWER), KC_SPC,      KC_ENT, MO(_RAISE), KC_RCTL, KC_RALT, KC_RGUI
+[_SYM] = LAYOUT(
+  KC_F1,   KC_F2, KC_F3,      KC_F4,   KC_F5,   KC_F6,                           KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+  KC_TILD,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                         KC_CIRC, KC_AMPR, KC_ASTR, KC_PLUS, KC_PIPE, KC_UNDS,
+  KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+  KC_NO,    KC_NO,   KC_NO,   KC_LPRN, KC_LBRC, KC_LCBR,  KC_NO,       KC_NO,    KC_RCBR, KC_RBRC, KC_RPRN, KC_EQL,  KC_BSLS, KC_NO,
+                     KC_LGUI, KC_LALT, KC_LCTL, KC_NO,    KC_TRNS,     KC_TRNS,  KC_NO,   KC_RCTL, KC_RALT, KC_RGUI
 ),
 /* RAISE
  * ,----------------------------------------.                     ,-----------------------------------------.
@@ -69,15 +70,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|-------.    ,-------|------+------+------+------+------+------|
  * |Shift | Undo |  Cut | Copy | Paste|      |       |    |       |      | LStr |      | LEnd |      | Shift|
  * `-----------------------------------------+-------|    |-------+-----------------------------------------'
- *               | LGUI | LAlt | LCTR |LOWER | Space |    | Enter |RAISE | RCTR | RAlt | RGUI |
+ *               | LGUI | LAlt | LCTR | SYM  | Space |    | Enter |RAISE | RCTR | RAlt | RGUI |
  *               `-----------------------------------'    `-----------------------------------'
  */
 [_RAISE] = LAYOUT(
-  KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,                           KC_NO,      KC_NO,    KC_NO,   KC_NO,    KC_NO,    KC_NO,
-  KC_NO,    KC_INS,  KC_PSCR, KC_APP,  KC_NO,    KC_NO,                           KC_PGUP,    KC_PRVWD, KC_UP,   KC_NXTWD, KC_DLINE, KC_BSPC,
-  KC_NO,    KC_LALT, KC_LCTL, KC_LSFT, KC_NO,    KC_CAPS,                         KC_PGDN,    KC_LEFT,  KC_DOWN, KC_RGHT,  KC_DEL,   KC_BSPC,
-  KC_LSFT,  KC_UNDO, KC_CUT,  KC_COPY, KC_PASTE, KC_NO,      KC_NO,       KC_NO,  KC_NO,      KC_LSTRT, KC_NO,   KC_LEND,  KC_NO,    KC_RSFT,
-                     KC_LGUI, KC_LALT, KC_LCTL,  MO(_LOWER), KC_SPC,      KC_ENT, MO(_RAISE), KC_RCTL, KC_RALT, KC_RGUI
+  KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,                         KC_NO,      KC_NO,    KC_NO,   KC_NO,    KC_NO,    KC_NO,
+  KC_NO,    KC_INS,  KC_PSCR, KC_APP,  KC_NO,    KC_NO,                         KC_PGUP,    KC_PRVWD, KC_UP,   KC_NXTWD, KC_DLINE, KC_BSPC,
+  KC_NO,    KC_LALT, KC_LCTL, KC_LSFT, KC_NO,    KC_CAPS,                       KC_PGDN,    KC_LEFT,  KC_DOWN, KC_RGHT,  KC_DEL,   KC_BSPC,
+  KC_LSFT,  KC_UNDO, KC_CUT,  KC_COPY, KC_PASTE, KC_NO,    KC_NO,       KC_NO,  KC_NO,      KC_LSTRT, KC_NO,   KC_LEND,  KC_NO,    KC_RSFT,
+                     KC_LGUI, KC_LALT, KC_LCTL,  MO(_SYM), KC_SPC,      KC_ENT, MO(_RAISE), KC_RCTL, KC_RALT, KC_RGUI
 ),
 };
 
@@ -121,8 +122,8 @@ static void print_status_narrow(void) {
         case _RAISE:
             oled_write_P(PSTR("Raise"), false);
             break;
-        case _LOWER:
-            oled_write_P(PSTR("Lower"), false);
+        case _SYM:
+            oled_write_P(PSTR("Sym"), false);
             break;
         default:
             oled_write_ln_P(PSTR("Undef"), false);
