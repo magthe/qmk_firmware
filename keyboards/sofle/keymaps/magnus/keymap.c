e7f44b369d6c6166da8b2bb808c39425977862d1
Magnus Therning <magnus@therning.org>
2023-09-17 07:42:32 +0200

    Make a first version of the navigation layer

--- keyboards/sofle/keymaps/magnus/keymap.c
+++ keyboards/sofle/keymaps/magnus/keymap.c
@@ -28,17 +28,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|-------.    ,-------|------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |       |    |       |   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------+-------|    |-------+-----------------------------------------'
- *               | LGUI | LAlt | LCTR |      |  SYM  |    |  SYM  |      | RCTR | LAlt | RGUI |
- *               |      |      |      |      | Space |    | Enter |      |      |      |      |
+ *               | LGUI | LAlt | LCTR | NAV  |  SYM  |    |  SYM  |      | RCTR | LAlt | RGUI |
+ *               |      |      |      | Esc  | Space |    | Enter |      |      |      |      |
  *               `-----------------------------------'    `-----------------------------------'
  */
 
 [_QWERTY] = LAYOUT(
-		   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                                             KC_6,     KC_7,    KC_8,    KC_9,   KC_0,     KC_BSLS,
-		   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                                             KC_Y,     KC_U,    KC_I,    KC_O,   KC_P,     KC_BSPC,
-		   KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                                             KC_H,     KC_J,    KC_K,    KC_L,   KC_SCLN,  KC_QUOT,
-		   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_NO,                   KC_NO,            KC_N,     KC_M,    KC_COMM, KC_DOT, KC_SLSH,  KC_RSFT,
-		                     KC_LGUI, KC_LALT, KC_LCTL, KC_NO, LT(_SYM, KC_SPC),        LT(_SYM, KC_ENT), MO(_NAV), KC_RCTL, KC_LALT, KC_RGUI
+		   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                                                        KC_6,     KC_7,    KC_8,    KC_9,   KC_0,     KC_BSLS,
+		   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                                                        KC_Y,     KC_U,    KC_I,    KC_O,   KC_P,     KC_BSPC,
+		   KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                                                        KC_H,     KC_J,    KC_K,    KC_L,   KC_SCLN,  KC_QUOT,
+		   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,             KC_NO,                   KC_NO,            KC_N,     KC_M,    KC_COMM, KC_DOT, KC_SLSH,  KC_RSFT,
+		                     KC_LGUI, KC_LALT, KC_LCTL, LT(_NAV, KC_ESC), LT(_SYM, KC_SPC),        LT(_SYM, KC_ENT), MO(_NAV), KC_RCTL, KC_LALT, KC_RGUI
 ),
 /* SYMBOLS
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -64,21 +64,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,----------------------------------------.                     ,-----------------------------------------.
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      |      |      |                    |      | PWrd |  Up  | NWrd | PgUp | Home |
+ * |      |      |      |      |      |      |                    |      |      |      |      | PgUp | Home |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |      |      |      |      |      |                    | Left | Down |  Up  | Rigth| PgDn | End  |
  * |------+------+------+------+------+------|-------.    ,-------|------+------+------+------+------+------|
  * |Shift |      |      |      |      |      |       |    |       |      |      |      |      |      |      |
  * `-----------------------------------------+-------|    |-------+-----------------------------------------'
- *               | LGUI | LAlt | LCTR |      |       |    |       |      |      |      |      |
+ *               | LGUI | LAlt | LCTR |  ▽  |       |    |       |      |      |      |      |
  *               `-----------------------------------'    `-----------------------------------'
  */
 [_NAV] = LAYOUT(
-  KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,                        KC_NO,   KC_NO,    KC_NO,   KC_NO,    KC_NO,    KC_NO,
-  KC_NO,    KC_INS,  KC_PSCR, KC_APP,  KC_NO,    KC_NO,                        KC_PGUP, KC_PRVWD, KC_UP,   KC_NXTWD, KC_DLINE, KC_BSPC,
-  KC_NO,    KC_LALT, KC_LCTL, KC_LSFT, KC_NO,    KC_CAPS,                      KC_PGDN, KC_LEFT,  KC_DOWN, KC_RGHT,  KC_DEL,   KC_BSPC,
-  KC_LSFT,  KC_UNDO, KC_CUT,  KC_COPY, KC_PASTE, KC_NO,   KC_NO,       KC_NO,  KC_NO,   KC_LSTRT, KC_NO,   KC_LEND,  KC_NO,    KC_RSFT,
-                     KC_LGUI, KC_LALT, KC_LCTL,  KC_NO,   KC_NO,       KC_NO,  KC_NO,   KC_RCTL, KC_RALT, KC_RGUI
+  KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+  KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_PGUP, KC_HOME,
+  KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,                        KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_PGDN, KC_END,
+  KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,       KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                KC_LGUI, KC_LALT, KC_LCTL, KC_TRNS, KC_NO,       KC_NO,  KC_NO,   KC_RCTL, KC_RALT, KC_RGUI
 ),
 };
 
