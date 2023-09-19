1f59576574e3362419384e9e9295d413e0b45e9c
Magnus Therning <magnus@therning.org>
2023-09-19 10:06:56 +0200

    Adjust the tapping timeout
    I wasn't happy with the retro tapping, it felt like it got in the way and didn't
    quite work as I'd hoped.

--- keyboards/sofle/keymaps/magnus/config.h
+++ keyboards/sofle/keymaps/magnus/config.h
@@ -5,4 +5,7 @@ see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
 for more options.
 */
 
-#define RETRO_TAPPING
+#ifdef TAPPING_TERM
+#undef TAPPING_TERM
+#endif
+#define TAPPING_TERM 225 // default: 100
