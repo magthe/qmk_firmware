9ec8f52ff37db62348254f9b5187dc179b5ba066
Magnus Therning <magnus@therning.org>
2023-10-27 10:16:05 +0200

    Remove ralt from the shift keys
    It was just confusing and caused too many typing mistakes. For now ralt will
    have to remain on tapping the lalt key.

--- keyboards/planck/keymaps/magnus/keymap.c
+++ keyboards/planck/keymaps/magnus/keymap.c
@@ -22,9 +22,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-----------------------------------------------------------------------------------'
    */
   [QWE] = LAYOUT_planck_1x2uC(
-      KC_DEL,          KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I,    KC_O,   KC_P,    KC_BSPC,
-      KC_TAB,          KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT,
-      LSFT_T(KC_RALT), KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_RALT),
+      KC_DEL,  KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I,    KC_O,   KC_P,    KC_BSPC,
+      KC_TAB,  KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT,
+      KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
       KC_CAPS, KC_LGUI, KC_LCTL, LALT_T(KC_RALT), LT(NAV,KC_ESC),
       LT(SYM,KC_SPC),
       KC_ENT, LALT_T(KC_RALT), KC_RCTL, KC_LGUI, LT(FUN,KC_APP)),
